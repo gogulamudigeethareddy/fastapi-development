c70d0606279da7bbe916dac21b8cf4ddd9e41cbc
geethareddy <gogulamudigeethareddy@gmail.com>
2024-03-08 18:00:15 -0600

    Updated

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},{"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 ##Path operation
 @app.get("/")   ##decorator
@@ -10,11 +26,22 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+@app.post("/posts")
+def create_posts(post:Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
+@app.get("/posts/{id}")
+def get_post(id:int):
+    post = find_post(id)
+    print(post)
+    return {"post_detail":post}
 
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts)-1]
+    return {"detail": post}
