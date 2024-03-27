c16189872276c757c0aaf1cf3825f7f0b052743d
geethareddy <gogulamudigeethareddy@gmail.com>
2024-03-26 21:16:50 -0500

    fixed import errord

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy .orm import Session
-from . import models
-from .database import engine, get_db
+import models
+from database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
